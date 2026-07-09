984d7417f24b52f98628f35a03d8bcd4278ff9d2
jeeh0272 <jeeh0272@gmail.com>
2026-07-09 19:32:47 -0300

    Update petrace.js
    Arrumei o options {

--- petrace.js
+++ petrace.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const { data, error } = await supabase.auth.signInWithOAuth({
                     provider: 'google',
                     options: {
-                        redirectTo: window.location.origin + '/index.html' 
+    redirectTo: window.location.origin + window.location.pathname.replace('petrace.html', 'index.html')
                     }
                 });
 
